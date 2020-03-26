6cef4b9a2ac1ad3c004c3b5968cfe9cb3f1edfa8
whichxjy <whichxjy@gmail.com>
2020-03-27 05:37:47 +0800

    Add text replacer

---
+++ src/util/regex.rs
@@ -0,0 +1,18 @@
+use regex::{Captures, Regex};
+
+pub fn replace_all(
+    pattern: &Regex,
+    haystack: &str,
+    replacer: impl Fn(&Captures) -> String,
+) -> String {
+    let mut new = String::with_capacity(haystack.len());
+    let mut last_match = 0;
+    for caps in pattern.captures_iter(haystack) {
+        let m = caps.get(0).unwrap();
+        new.push_str(&haystack[last_match..m.start()]);
+        new.push_str(&replacer(&caps));
+        last_match = m.end();
+    }
+    new.push_str(&haystack[last_match..]);
+    new
+}
