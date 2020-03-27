d321781b4796777fd4bcb58d6ce73fd02c15e478
whichxjy <whichxjy@gmail.com>
2020-03-27 19:14:09 +0800

    Add client

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 
 #[macro_use]
 mod util;
+mod client;
 mod boilerplate;
 mod config;
 mod html;
@@ -10,35 +11,6 @@ mod line;
 mod metadata;
 mod spec;
 
-use clap::{App, Arg, SubCommand};
-use spec::Spec;
-
-fn handle_spec(infile: &str) {
-    let mut doc = Spec::new(infile);
-    doc.preprocess();
-}
-
 fn main() {
-    let matches = App::new("Bikeshed Demo")
-        .version("1.0")
-        .author("whichxjy")
-        .subcommand(
-            SubCommand::with_name("spec")
-                .about("process a spec source file into a valid output file")
-                .arg(
-                    Arg::with_name("infile")
-                        .required(true)
-                        .takes_value(true)
-                        .help("path to the source file")
-                        .index(1),
-                ),
-        )
-        .get_matches();
-
-    let infile = matches
-        .subcommand_matches("spec")
-        .unwrap()
-        .value_of("infile")
-        .unwrap();
-    handle_spec(infile);
+    client::run();
 }
