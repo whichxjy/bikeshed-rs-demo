d8a7b1c863c4097d55761b9442702ff002d3cebd
whichxjy <whichxjy@gmail.com>
2020-05-12 15:41:56 +0800

    Update spec subcommand

--- src/client.rs
+++ src/client.rs
@@ -19,26 +19,26 @@ fn handle_spec(matches: ArgMatches) {
 }
 
 pub fn run() {
-    let matches = App::new("Bikeshed Demo")
+    let spec_subcommand = SubCommand::with_name("spec")
+        .about("Process a spec source file into a valid output file")
+        .arg(
+            Arg::with_name("infile")
+                .required(true)
+                .takes_value(true)
+                .help("path to the source file")
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("outfile")
+                .takes_value(true)
+                .help("path to the output file")
+                .index(2),
+        );
+
+    let matches = App::new("Bikeshed.rs Demo")
         .version("1.0")
         .author("whichxjy")
-        .subcommand(
-            SubCommand::with_name("spec")
-                .about("Process a spec source file into a valid output file")
-                .arg(
-                    Arg::with_name("infile")
-                        .required(true)
-                        .takes_value(true)
-                        .help("path to the source file")
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("outfile")
-                        .takes_value(true)
-                        .help("path to the output file")
-                        .index(2),
-                ),
-        )
+        .subcommand(spec_subcommand)
         .get_matches();
 
     match matches.subcommand_name() {
